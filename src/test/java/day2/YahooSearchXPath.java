58e858ee69d18b1cdcd93660cf269ec02b7dd3eb
maksymseliukov <maks.seli.qa@gmail.com>
2019-03-26 23:31:50 -0500

    Add YahooSearchXPath

---
+++ src/test/java/day2/YahooSearchXPath.java
@@ -0,0 +1,93 @@
+package day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class YahooSearchXPath {
+
+    WebDriver driver;
+
+    By searchInput = By.xpath("//*[@name='p']");
+                           // "//*[@name='p']"
+
+    @BeforeSuite
+    public void suitSetup() {
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/test/resources/geckodriver");
+        driver = new FirefoxDriver();
+        //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void testSearch() {
+        String queryString = "Portnov School";
+
+        openMainPage();
+        typeQuery(queryString);
+        submitSearch();
+        assertResults();
+
+    }
+
+    @Test
+    public void testSearch02() {
+        String queryString = "Portnov Computer School";
+
+        openMainPage();
+        typeQuery(queryString);
+        submitSearch();
+        assertResults();
+
+    }
+
+    private void assertResults() {
+        WebElement resultStatsElement = driver.findElement(By.xpath("//*[contains(@id,'yui')]")); //  //*[contains(@name,'rest')]
+        System.out.println(resultStatsElement.isDisplayed());
+        //String textFromResults = resultStatsElement.getText();
+        //String xpathValue = "//*[contains(@id,'yui')]";
+        //explicitWait(driver, By.xpath(xpathValue));
+        //Assert.assertTrue(textFromResults.endsWith("results"));
+
+
+
+    }
+
+    //private void explicitWait (WebDriver driver, By element) {
+       // WebDriverWait wait = new WebDriverWait(driver, 10);
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(element));
+    //}
+
+    private void submitSearch() {
+        WebElement element = driver.findElement(searchInput);
+        element.submit();
+    }
+
+    private void typeQuery(String queryString) {
+        WebElement element = driver.findElement(searchInput);
+        element.sendKeys(queryString);
+
+    }
+
+    private void openMainPage() {
+        driver.get("https://www.yahoo.com/");
+    }
+
+    //public void explicitWait(WebDriver driver){
+       // WebDriverWait wait = new WebDriverWait(driver, 10);
+       // wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@id,'yui')]")));
+
+    //}
+
+    public void sleepThread(){
+       try {
+           Thread.sleep(3000);
+        } catch (InterruptedException e) {
+           e.printStackTrace();
+      }
+   }
+
+}
